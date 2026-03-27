4c95f0e765a72a56867c7b8f71f232bc33bdf25f
Kai <nebula7458@proton.me>
2026-03-26 20:12:22 -0400

    fix: add Helius RPC to CSP connect-src

--- app/next.config.js
+++ app/next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https://raw.githubusercontent.com https://arweave.net https://assets.coingecko.com https://coin-images.coingecko.com https://static.jup.ag https://tokens.jup.ag https://metadata.jito.network https://pyth.network https://wormhole.com https://ipfs.io https://*.ipfs.cf-ipfs.com https://*.ipfs.nftstorage.link https://shdw-drive.genesysgo.net https://gateway.irys.xyz https://api.coingecko.com",
-              "connect-src 'self' https://api.devnet.solana.com https://api.mainnet-beta.solana.com https://hermes.pyth.network https://benchmarks.pyth.network https://api.jup.ag https://api.coingecko.com wss://api.devnet.solana.com wss://api.mainnet-beta.solana.com",
+              "connect-src 'self' https://api.devnet.solana.com https://api.mainnet-beta.solana.com https://mainnet.helius-rpc.com https://hermes.pyth.network https://benchmarks.pyth.network https://api.jup.ag https://api.coingecko.com wss://api.devnet.solana.com wss://api.mainnet-beta.solana.com wss://mainnet.helius-rpc.com",
               "frame-ancestors 'none'",
               "base-uri 'self'",
               "form-action 'self'",
